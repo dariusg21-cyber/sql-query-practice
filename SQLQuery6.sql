19915152d57d466757a9919b506b6567478eb86d
dariusg21-cyber <81822497+dariusg21-cyber@users.noreply.github.com>
2023-05-31 08:57:51 -0500

    Update SQLQuery6.sql

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -2,3 +2,41 @@ select LastName, FirstName, Description  from
 work
  join ARTIST on 
 work.ArtistID = artist.ArtistID;
+
+OUTPUT
+
+LastName	FirstName	Description
+Horiuchi                 	Paul                     	31 x 24.8 in.
+Graves                   	Morris                   	Northwest School Expressionist style
+Miro                     	Joan                     	Early Surrealist style
+Miro                     	Joan                     	Surrealist style
+Kandinsky                	Wassily                  	Bauhaus style of Kandinsky
+Matisse                  	Henri                    	A very colorful Impressionist piece
+Tobey                    	Mark                     	Signed
+Graves                   	Morris                   	50 x 72.5 cm. - Signed
+Kandinsky                	Wassily                  	The Blue Rider-Early Pointilism influence
+Klee                     	Paul                     	Bauhaus style of Klee
+Matisse                  	Henri                    	An Impressionist masterpiece
+Chagall                  	Marc                     	Shows Belarusian folk-life themes and symbology
+Sargent                  	John Singer              	Shows French Impressionist influence of Monet
+Graves                   	Morris                   	33.3 x 16.1 cm. - Signed
+Chagall                  	Marc                     	Shows Belarusian folk-life themes and symbology
+Sargent                  	John Singer              	American realist style - From work in Spain
+Tobey                    	Mark                     	Northwest School Abstract Expressionist style
+Tobey                    	Mark                     	Northwest School Abstract Expressionist style
+Horiuchi                 	Paul                     	Northwest School Abstract Expressionist style
+Tobey                    	Mark                     	4.3 x 6.1 in. Signed
+Horiuchi                 	Paul                     	71 x 78 in. - Signed
+Graves                   	Morris                   	Northwest School Expressionist style
+Tobey                    	Mark                     	19.3 x 24.4 in. - Signed
+Tobey                    	Mark                     	19.3 x 24.4 in. - Signed
+Chagall                  	Marc                     	Shows Belarusian folk-life themes and symbology
+Sargent                  	John Singer              	American Realist style - From work in Spain
+Tobey                    	Mark                     	Northwest School Abstract Expressionist style
+Tobey                    	Mark                     	Northwest School Abstract Expressionist style
+Horiuchi                 	Paul                     	Northwest School Abstract Expressionist style
+Tobey                    	Mark                     	43.9 x 28 in.
+Graves                   	Morris                   	26.5 x 29.75 in. - Signed
+Graves                   	Morris                   	Northwest School Expressionist style
+Graves                   	Morris                   	Northwest School Expressionist style
+Graves                   	Morris                   	Northwest School Expressionist style
